5c2df7179f01778b6ee2e5c5bc826fb8736f61cd
Kohidad <kohidad@proton.me>
2024-09-19 17:25:15 +0700

    Added writing account to files, checking duplicate username

--- Lab22/app/src/main/java/com/example/lab2_2/SignUpActivity.java
+++ Lab22/app/src/main/java/com/example/lab2_2/SignUpActivity.java
@@ -12,6 +12,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText user_username;
@@ -21,6 +26,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private Button btnSignUp;
 
     private final String REQUIRE = "Required";
+    private final String USERBASE = "userbase.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +40,72 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         accountExist = findViewById(R.id.accountExist);
         btnSignUp = findViewById(R.id.btnSignUp);
 
+        //This means pointing to the current class (SignUpActivity)
         accountExist.setOnClickListener(this);
         btnSignUp.setOnClickListener(this);
     }
 
-    private  boolean checkInput(){
-        if (TextUtils.isEmpty(user_username.getText().toString())){
+    public void writeToFile(String fileName, String content) {
+        //Return the context of the entire application. Not current activity (diff from getContext())
+        //This means pointing to the current class (SignUpActivity)
+        File path = this.getFilesDir();
+        try {
+            //append to avoid overwrite
+            FileOutputStream writer = new FileOutputStream(new File(path, fileName), true);
+            writer.write((content + "\n").getBytes());
+            writer.close();
+            Toast.makeText(this, "Wrote to file: " + fileName, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean findAccount(String fileName, String username) {
+        File path = this.getFilesDir();
+        File readFile = new File(path, fileName);
+        //Allocate memory based on file size.
+        byte[] content = new byte[(int) readFile.length()];
+
+        //Check if file exist, else write regardless.
+        if (!readFile.exists()){
+            return true;
+        }
+
+        try {
+            FileInputStream stream = new FileInputStream(readFile);
+            stream.read(content);
+            stream.close();
+
+            //Turn the byte[] into a string
+            String fileContent = new String(content);
+
+            String userInfo = username + ":";
+
+            if (fileContent.contains(userInfo)){
+                Log.i("UserDuplicate","User creation duplicated found, denied entry user: " + username);
+                return false;
+            }
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, "File not found!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    private boolean checkInput() {
+        if (TextUtils.isEmpty(user_username.getText().toString())) {
             user_username.setError(REQUIRE);
             return false;
         }
 
-        if (TextUtils.isEmpty(user_password.getText().toString())){
+        if (TextUtils.isEmpty(user_password.getText().toString())) {
             user_username.setError(REQUIRE);
             return false;
         }
 
-        if (TextUtils.isEmpty(user_password.getText().toString())){
+        if (TextUtils.isEmpty(user_password.getText().toString())) {
             user_username.setError(REQUIRE);
             return false;
         }
@@ -59,17 +115,25 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             return false;
         }
 
-        Log.i("UserSignUp","A user has successfully created an account.");
+        if (!findAccount(USERBASE,user_username.getText().toString())){
+            Toast.makeText(this, "Username already exists!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        Log.i("UserSignUp", "User " + user_username.getText().toString() + " has successfully created an account.");
         return true;
     }
 
-    private void signUp(){
-        if (!checkInput()){
+    private void signUp() {
+        if (!checkInput()) {
             return;
         }
+
+        String userAccountInfo = user_username.getText().toString() + ":" + user_password.getText().toString();
+        writeToFile(USERBASE, userAccountInfo);
     }
 
-    private void signInForm(){
+    private void signInForm() {
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
         finish();
@@ -79,9 +143,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View view) {
         int id = view.getId();
 
-        if (id == R.id.btnSignUp){
+        if (id == R.id.btnSignUp) {
             signUp();
-        } else if (id == R.id.accountExist){
+        } else if (id == R.id.accountExist) {
             signInForm();
         }
     }
